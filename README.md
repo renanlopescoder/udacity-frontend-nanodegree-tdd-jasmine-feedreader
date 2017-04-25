adefe5518bd3cb48892362ce4fcab16bcfdbc9a7
Renan Lopes <renanlopescoder@gmail.com>
2017-04-25 19:50:30 -0300

    Feat: JavaScript TDD Jasmine

---
+++ README.md
@@ -0,0 +1,48 @@
+### JavaScript TDD com Jasmine (Test Driven Development)
+
+##### Front End Web Developer Nanodegree por Udacity
+###### Testes  Aplicados por Renan Lopes
+
+#### Executando o projeto
+
+- Download do arquivo zip ou clone do repositório
+- Acesse o diretório de download e descompacte o arquivo
+- Abra o arquivo index.html e os testes serão executados
+
+#### Recursos do Projeto
+
+- Jasmine Framework
+
+#### Testes Aplicados
+
+- Aplicado teste para verificar se as URL's dos Feeds foram definidas
+- Aplicado teste para verificar se os nomes dos Feeds foram definidos
+- Aplicado teste para verificar se o menu está oculto após carregamento por padrão
+- Aplicado teste para verificar o funcionamento do botão de menu
+- Aplicado teste para verificar o carregamento dos feeds
+- Aplicado teste para verificar se os feeds realmente foram carregados
+
+### English
+
+### JavaScript TDD with Jasmine (Test Driven Development)
+##### Front End Web Developer Nanodegree by Udacity
+###### Tests Applied by Renan Lopes
+
+#### Running the Project
+
+- Download zip file or clone the repository
+- Access the download directory and unzip the file
+- Open the index.html file and all tests will run
+
+#### Project Features
+
+- Jasmine Framework
+
+#### Applied Tests
+
+- Applied test to check if the URLs of the Feeds have been defined
+- Applied test to check if Feeds names have been defined
+- Applied test to check if the menu is hidden after loading by default
+- Applied test to check how the menu button works
+- Applied test to check feed loading
+- Applied test to verify that feeds were actually loaded
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,100 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+
+$(function() {
+    describe('RSS Feeds', function() {
+
+        it('Feeds are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        it('Feed url defined', function () {
+            let urlDefined;
+            for(let i = 0; i < allFeeds.length ; i++){
+                if(allFeeds[i].url == '' || allFeeds[i].url == undefined) {
+                    urlDefined = false
+                    break;
+                }
+                else {
+                    urlDefined = true;
+                }
+            };
+            expect(urlDefined).toBe(true);
+        });
+
+        it('Feed name defined', function () {
+            let nameDefined;
+            for(let i = 0; i < allFeeds.length ; i++){
+                if(allFeeds[i].name == '' || allFeeds[i].name == undefined) {
+                    nameDefined = false;
+                    break;
+                } else {
+                    nameDefined = true;
+                }
+            };
+            expect(nameDefined).toBe(true);
+        });
+        
+    });
+
+    describe('The menu', function() {
+
+        let body = $('body');
+
+        it('Hidden menu', function () {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+
+        it('menu button', function () {
+            let menuIcon = $('.menu-icon-link');
+
+            menuIcon.click();
+            expect(body.hasClass('menu-hidden')).toBe(false);
+
+            menuIcon.click();
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
+    describe('Initial Entries', function() {
+
+        beforeEach(function(done){
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('Has at least one entry after loadFeed()', function (done) {
+            let feedEntry = $('.entry').length;
+            expect(feedEntry).not.toBe(0);
+            done();
+        });
+    });
+
+    describe('New feed selection', function() {
+        let initFeed;
+        beforeEach(function(done){
+            loadFeed(0, function() {
+                initFeed = $('.feed').innerHTML;
+                loadFeed(1, function(){
+                    done(); 
+                });
+            });
+        });
+
+        it('Loaded content', function (done) {
+            let newFeedSelection = $('.feed').innerHTML;
+            expect(initFeed).not.toBe(newFeedSelection);
+            done();
+        });
+    });
+}());
