c7fb992cef060485e647d441065040eb39d3aa0c
Renan Lopes <renanlopescoder@gmail.com>
2017-04-26 08:47:31 -0300

    fix: Descendant Selector
    - Fix descendant selector with jQuery
    - Add comments for each test
    - Improve the test to check url  "feed"
    - Improve the test to check "feed" name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,6 +10,11 @@
  */
 
 $(function() {
+
+    /**
+     * Feed Tests
+     */
+
     describe('RSS Feeds', function() {
 
         it('Feeds are defined', function() {
@@ -17,43 +22,50 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+        /**
+         * Test to check if the URLs of the Feeds have been defined
+         */
+
         it('Feed url defined', function () {
-            let urlDefined;
-            for(let i = 0; i < allFeeds.length ; i++){
-                if(allFeeds[i].url == '' || allFeeds[i].url == undefined) {
-                    urlDefined = false
-                    break;
-                }
-                else {
-                    urlDefined = true;
-                }
-            };
-            expect(urlDefined).toBe(true);
+            allFeeds.forEach(function (feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
         });
 
+        /**
+         * Test to check if Feeds names have been defined
+         */
+
         it('Feed name defined', function () {
-            let nameDefined;
-            for(let i = 0; i < allFeeds.length ; i++){
-                if(allFeeds[i].name == '' || allFeeds[i].name == undefined) {
-                    nameDefined = false;
-                    break;
-                } else {
-                    nameDefined = true;
-                }
-            };
-            expect(nameDefined).toBe(true);
+            allFeeds.forEach(function (feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
         });
         
     });
+
+    /**
+     * Menu Tests
+     */
 
     describe('The menu', function() {
 
         let body = $('body');
 
+        /**
+         * Test to check if the menu is hidden after loading by default
+         */
+
         it('Hidden menu', function () {
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
 
+        /**
+         * Test to check how the menu button works
+         */
+        
         it('menu button', function () {
             let menuIcon = $('.menu-icon-link');
 
@@ -65,6 +77,10 @@ $(function() {
         });
     });
 
+    /**
+     * Test to check feed loading
+     */
+
     describe('Initial Entries', function() {
 
         beforeEach(function(done){
@@ -74,11 +90,15 @@ $(function() {
         });
 
         it('Has at least one entry after loadFeed()', function (done) {
-            let feedEntry = $('.entry').length;
+            let feedEntry = $('.feed .entry').length;
             expect(feedEntry).not.toBe(0);
             done();
         });
     });
+
+    /**
+     * Test to verify that feeds were actually loaded
+     */
 
     describe('New feed selection', function() {
         let initFeed;
@@ -86,15 +106,14 @@ $(function() {
             loadFeed(0, function() {
                 initFeed = document.querySelector('.feed').innerHTML;
                 loadFeed(1, function(){
-                    done(); 
+                    done();
                 });
             });
         });
 
-        it('Loaded content', function (done) {
+        it('Loaded content', function () {
             let newFeedSelection = document.querySelector('.feed').innerHTML;
             expect(initFeed).not.toBe(newFeedSelection);
-            done();
         });
     });
 }());
